3be6c536fa1a30d948ab8f56b15ff6eb7d0dd45d
elcio <elciolucena@gmail.com>
2026-01-20 13:07:47 -0300

    Alteração no código python

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print_hi("Elcio Lucena")
+    print_hi("Elcio Costa Lucena")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
